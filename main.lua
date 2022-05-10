9434d0c06683f40669cc79898c3273275bf82edc
leg1337 <88927991+leg1337@users.noreply.github.com>
2022-05-10 18:51:21 +0300

    update lol
    -- added extra commands
    
    -- got ping to work (now you can see your own ping) (credits to marley i think for this i didn't know how to get it to work
    
    -- kill is pretty decent let me know what i can add next

--- main.lua
+++ main.lua
@@ -2,6 +2,17 @@
 -- https://goonlinetools.com/lua-beautifier/ is really cool
 
 --[ Notification System ]
+function notification(x, y, time)
+    game.StarterGui:SetCore(
+        "SendNotification",
+        {
+            Title = x,
+            Text = y,
+            Duration = time
+        }
+    )
+end
+
 game:service [[StarterGui]]:SetCore(
     [[SendNotification]],
     {
@@ -13,10 +24,10 @@ game:service [[StarterGui]]:SetCore(
 --[ Local Variables ]
 local plr = game.Players.LocalPlayer
 local Prefix = ">"
-local TP = game:GetService[[TeleportService]]
+local TP = game:GetService [[TeleportService]]
 local Service = game:GetService [[Players]]
-local LP = game:GetService[[Players]].LocalPlayer
-local CF = game:GetService[[Players]].LocalPlayer.Character.HumanoidRootPart.CFrame
+local LP = game:GetService [[Players]].LocalPlayer
+local CF = game:GetService [[Players]].LocalPlayer.Character.HumanoidRootPart.CFrame
 local chr = game.Players.LocalPlayer.Character
 local Tool = plr.Backpack:FindFirstChildOfClass("Tool")
 local Humanoid = plr.Character.Humanoid:Clone()
@@ -104,13 +115,14 @@ plr.Chatted:Connect(
                         v.Name:lower():sub(1, #args[2]) == args[2]:lower() or
                             v.DisplayName:lower():sub(1, #args[2]) == args[2]:lower()
                      then
-                        local CF = game:GetService[[Players]].LocalPlayer.Character.HumanoidRootPart.CFrame
+                        local CF = game:GetService [[Players]].LocalPlayer.Character.HumanoidRootPart.CFrame
                         local Tool = plr.Backpack:FindFirstChildOfClass("Tool")
                         local Humanoid = plr.Character.Humanoid:Clone()
                         plr.Character.Humanoid:UnequipTools()
                         plr.Character.Animate.Disabled = true
                         plr.Character.Humanoid:Destroy()
                         Humanoid.Parent = plr.Character
+                        task.wait(game:GetService("Players").RespawnTime - 0.3)
                         Tool.Parent = plr.Character
                         firetouchinterest(Tool.Handle, v.Character.HumanoidRootPart, 0)
                         LP.CharacterAdded:wait()
@@ -131,8 +143,60 @@ plr.Chatted:Connect(
     end
 )
 
-while task.wait(2.1) do
-print [[Kekware on top]]
-end
+plr.Chatted:Connect(
+    function(msg)
+        local args = msg:lower():split(" ")
+        print(args[1], args[2])
+        if args[1] == Prefix .. "kill" then
+            for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+                if v.Character then
+                    if
+                        v.Name:lower():sub(1, #args[2]) == args[2]:lower() or
+                            v.DisplayName:lower():sub(1, #args[2]) == args[2]:lower()
+                     then
+                        local Player = game.Players.LocalPlayer
+                        local LP = game:GetService [[Players]].LocalPlayer
+                        local CF = game:GetService [[Players]].LocalPlayer.Character.HumanoidRootPart.CFrame
+                        local Character = Player.Character
+                        local Tool = Player.Backpack:FindFirstChildOfClass("Tool")
+                        local ClonedHum = Character.Humanoid:Clone()
+                        workspace["FallenPartsDestroyHeight"] = 0 / 0
+                        ClonedHum.Parent = Character
+                        Character.Humanoid:Destroy()
+                        ClonedHum:EquipTool(Tool)
+                        firetouchinterest(Tool.Handle, v.Character.HumanoidRootPart, 0)
+                        ClonedHum:ChangeState(15)
+                        LP.CharacterAdded:wait()
+                        LP.Character:WaitForChild [[HumanoidRootPart]].CFrame = CF
+                        local FF = LP.Character:WaitForChild [[ForceField]]
+                        FF:Destroy()
+                        game:service [[StarterGui]]:SetCore(
+                            [[SendNotification]],
+                            {
+                                Title = [[Target Killed]],
+                                Text = [[Kekware v1.02]]
+                            }
+                        )
+                    end
+                end
+            end
+        end
 
+        plr.Chatted:Connect(
+            function(message)
+                local loweredString = string.lower(message)
+                local args = string.split(loweredString, " ")
+                if args[1] == Prefix .. [[ping]] then
+                    local ping =
+                        ("Pong! " .. ("%.f"):format(game:GetService("Stats").PerformanceStats.Ping:GetValue()) .. "ms") -- credit to whoever this belongs to
 
+                    notification("" .. ping, "Kekware v1.02", 2)
+                end
+            end
+        )
+
+        while task.wait(5) do
+            print [[Kekware on top]]
+        end
+    end
+)
